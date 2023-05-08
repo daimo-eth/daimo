24bd59f06cebe6c29d5876254c259fa0c07fecc9
DC <dcposch@dcpos.ch>
2023-05-08 16:15:23 +0200

    Recommended extensions

--- packages/daimo-mobile/babel.config.js
+++ packages/daimo-mobile/babel.config.js
@@ -1,6 +1,8 @@
+/** @type {import('@babel/core').ConfigFunction} */
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins: [require.resolve("expo-router/babel")],
   };
 };
--- packages/daimo-mobile/metro.config.js
+++ packages/daimo-mobile/metro.config.js
@@ -1,8 +1,7 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
 
-// eslint-disable-next-line no-undef
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname); // eslint-disable-line no-undef
 
 config.resolver = { ...config.resolver, unstable_enablePackageExports: true };
 
