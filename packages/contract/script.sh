745e504efe1907d421d4194c4f8a0ae4262eb66a
DC <dcposch@dcpos.ch>
2023-06-03 01:45:09 -0700

    Deploy NameRegistry

--- packages/contract/script.sh
+++ packages/contract/script.sh
@@ -10,3 +10,6 @@ forge script script/NewAccount.s.sol --fork-url $RPC_URL --private-key $PRIVATE_
 forge script script/Testing.s.sol --fork-url $RPC_URL --private-key $PRIVATE_KEY
 # Create calldata for UserOp for bundler
 cast cd "execute(address,uint256,bytes)" 0xF05b5f04B7a77Ca549C0dE06beaF257f40C66FDB 10000000000000000 0x
+
+# Deploy name registry
+forge script script/DeployNameRegistry.s.sol --fork-url $RPC_URL --private-key $PRIVATE_KEY --broadcast --verify  --etherscan-api-key $ETHERSCAN_API_KEY
