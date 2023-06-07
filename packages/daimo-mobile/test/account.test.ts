0fb0e103c6827f24cb9a4b18911dcf491d2db900
DC <dcposch@dcpos.ch>
2023-06-07 03:00:37 -0700

    Fix Github CI

--- packages/daimo-mobile/test/account.test.ts
+++ packages/daimo-mobile/test/account.test.ts
@@ -3,6 +3,7 @@ import { Account, parse, serialize } from "../src/logic/account";
 describe("Account", () => {
   it("serializes", () => {
     const a: Account = {
+      name: "test",
       address: "0x123",
       lastBalance: BigInt(123),
       lastNonce: BigInt(456),
@@ -10,15 +11,16 @@ describe("Account", () => {
     };
     const ser = serialize(a);
     expect(ser).toEqual(
-      '{"storageVersion":1,"address":"0x123","lastBalance":"123","lastNonce":"456","lastBlockTimestamp":789}'
+      '{"storageVersion":1,"name":"test","address":"0x123","lastBalance":"123","lastNonce":"456","lastBlockTimestamp":789}'
     );
   });
 
   it("deserializes", () => {
     const ser =
-      '{"storageVersion":1,"address":"0x123","lastBalance":"123","lastNonce":"456","lastBlockTimestamp":789}';
+      '{"storageVersion":1,"name":"test","address":"0x123","lastBalance":"123","lastNonce":"456","lastBlockTimestamp":789}';
     const a = parse(ser);
     expect(a).toEqual({
+      name: "test",
       address: "0x123",
       lastBalance: BigInt(123),
       lastNonce: BigInt(456),
