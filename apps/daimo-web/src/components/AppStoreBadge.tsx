319d7383069b19e6b409cf6308833f0ebabb94b0
Nalin Bhardwaj <nalinbhardwaj@nibnalin.me>
2023-07-26 00:37:06 +0100

    web: link form from app store badges directly

--- apps/daimo-web/src/components/AppStoreBadge.tsx
+++ apps/daimo-web/src/components/AppStoreBadge.tsx
@@ -1,18 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 
 /**
  * Show the app store badge for iOS or Android.
  */
 export function AppStoreBadge({ platform }: { platform: "ios" | "android" }) {
-  const url = "javascript:alert('Coming soon')"; // eslint-disable-line
+  const url = "https://notionforms.io/forms/daimo-uk2fe4";
   const image =
     platform === "ios" ? "/badge-app-store.svg" : "/badge-play-store.svg";
   const alt =
     platform === "ios" ? "Download on the App Store" : "Get it on Google Play";
 
   return (
-    <a href={url}>
+    <Link href={url} target="_blank">
       <Image src={image} alt={alt} width={140} height={42} />
-    </a>
+    </Link>
   );
 }
