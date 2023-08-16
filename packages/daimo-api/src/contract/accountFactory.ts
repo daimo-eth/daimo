ddabfcbba90c948f3fa77d355264a5521f135ece
Nalin Bhardwaj <nalinbhardwaj@nibnalin.me>
2023-08-15 17:17:20 -0700

    Move deploy from first userop to backend

--- packages/daimo-api/src/contract/accountFactory.ts
+++ packages/daimo-api/src/contract/accountFactory.ts
@@ -1,17 +1,16 @@
 import { accountFactoryConfig } from "@daimo/contract";
-import { Hex, TransactionReceipt } from "viem";
+import { Hex } from "viem";
 
 import { ViemClient } from "../chain";
 
 /* Interface to the AccountFactory contract. Creates Daimo accounts. */
 export class AccountFactory {
   constructor(private vc: ViemClient) {}
-
   /**
    * Takes a DER P256 public key.
-   * Returns the contract friendly raw key args.
+   * Deploys a new Daimo account with that as it's initial signing key.
    */
-  computeContractArgs = (pubKeyHex: string) => {
+  async deploy(pubKeyHex: string) {
     const derPrefix =
       "0x3059301306072a8648ce3d020106082a8648ce3d03010703420004";
     if (!pubKeyHex.startsWith(derPrefix)) {
@@ -27,39 +26,27 @@ export class AccountFactory {
     const key2 = `0x${pubKey.subarray(32).toString("hex")}` as Hex;
     const salt = 0n;
     const args = [[key1, key2], salt] as const;
-    return args;
-  };
 
-  /**
-   * Takes a DER P256 public key.
-   * Returns the CREATE2 deployment address for Daimo account with that as it's initial signing key.
-   * We don't use Entrypoint's getSenderAddress because it's too complicated. Daimo Accounts have a
-   * simpler getAddress function that we can use instead.
-   */
-  async getAddress(pubKeyHex: string) {
-    const address = await this.vc.publicClient.readContract({
-      ...accountFactoryConfig,
-      functionName: "getAddress",
-      args: this.computeContractArgs(pubKeyHex),
-    });
-    return address;
-  }
-
-  /**
-   * Takes a DER P256 public key.
-   * Deploys a new Daimo account with that as it's initial signing key.
-   */
-  async deploy(pubKeyHex: string): Promise<TransactionReceipt> {
+    // Deploy account
     const hash = await this.vc.walletClient.writeContract({
       ...accountFactoryConfig,
       functionName: "createAccount",
-      args: this.computeContractArgs(pubKeyHex),
+      args,
     });
     console.log(`[API] deploy transaction ${hash}`);
-    const receipt = await this.vc.publicClient.waitForTransactionReceipt({
-      hash,
+    const tx = await this.vc.publicClient.waitForTransactionReceipt({ hash });
+    console.log(`[API] deploy transaction ${tx.status}`);
+    if (tx.status !== "success") {
+      return { status: "reverted" as const, address: undefined };
+    }
+
+    // Compute CREATE2 deployment address
+    const address = await this.vc.publicClient.readContract({
+      ...accountFactoryConfig,
+      functionName: "getAddress",
+      args,
     });
-    console.log(`[API] deploy transaction status ${receipt.status}`);
-    return receipt;
+
+    return { status: "success" as const, address };
   }
 }
