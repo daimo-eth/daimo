1d63e596808d64411bbe2826f932f760b4aab0c2
DC <dc@dcpos.ch>
2023-08-18 00:43:17 -0700

    perf: faster Create Note

--- apps/daimo-mobile/src/logic/note.ts
+++ apps/daimo-mobile/src/logic/note.ts
@@ -1,88 +1,15 @@
-import * as Contracts from "@daimo/contract";
+import { formatDaimoLink } from "@daimo/common";
 import { useEffect, useState } from "react";
-import {
-  Address,
-  Hex,
-  PublicClient,
-  Transport,
-  createPublicClient,
-  createWalletClient,
-  http,
-  keccak256,
-} from "viem";
+import { Address, Hex, createWalletClient, http, keccak256 } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import { baseGoerli } from "viem/chains";
 
-import { chainConfig } from "./chainConfig";
+import { rpcHook } from "./trpc";
 
-export type EphemeralNote = {
-  owner: Address;
-  from: Address;
-  amount: bigint;
-};
-
-type EphemeralNoteLoadState = "loading" | "error" | "loaded";
-
-// TODO: remove eth JSON RPC dependency
-// Notes can be loaded thru the API to miminize round trips
-let clientL2: PublicClient<Transport, typeof baseGoerli> | undefined;
-function getClientL2() {
-  if (!clientL2) {
-    clientL2 = createPublicClient({ chain: chainConfig.l2, transport: http() });
-  }
-  return clientL2;
-}
-
-export function useFetchNote(
-  ephemeralOwner: `0x${string}`
-): [EphemeralNote | undefined, EphemeralNoteLoadState] {
-  const [note, setNote] = useState<EphemeralNote | undefined>(undefined);
-  const [loadState, setLoadState] = useState<EphemeralNoteLoadState>("loading");
-
-  useEffect(() => {
-    (async () => {
-      const note = await fetchNote(ephemeralOwner);
-      console.log(`[NOTE] fetched note ${ephemeralOwner}: ${note?.amount}`);
-      setNote(note);
-      if (note) setLoadState("loaded");
-      else setLoadState("error");
-    })();
-  }, [ephemeralOwner]);
-
-  return [note, loadState];
-}
-
-async function fetchNote(
-  ephemeralOwner: `0x${string}`
-): Promise<EphemeralNote | undefined> {
-  const res = await getClientL2().readContract({
-    ...Contracts.ephemeralNotesConfig,
-    functionName: "notes",
-    args: [ephemeralOwner],
-  });
-
-  if (res[0] !== ephemeralOwner) {
-    return undefined;
-  } else {
-    return {
-      owner: res[0],
-      from: res[1],
-      amount: res[2],
-    };
-  }
-}
-
-export async function fetchNotesContractAllowance(
-  address: Address
-): Promise<bigint> {
-  const allowance = await getClientL2().readContract({
-    abi: Contracts.erc20ABI,
-    address: Contracts.tokenMetadata.address,
-    functionName: "allowance",
-    args: [address, Contracts.ephemeralNotesAddress],
-  });
-
-  return allowance;
+export function useFetchNote(ephemeralOwner: `0x${string}`) {
+  const url = formatDaimoLink({ type: "note", ephemeralOwner });
+  const ret = rpcHook.getLinkStatus.useQuery({ url });
+  return ret;
 }
 
 export function useEphemeralSignature(
@@ -115,5 +42,6 @@ export function useEphemeralSignature(
   return ephemeralSignature;
 }
 
+// TODO: remove after we migrate to EIP-7212 contract
 // Dummy signature of correct length and valid r/s values (so ECDSA.sol doesn't revert)
 const dummySignature: `0x${string}` = `0x001d2a4239a139c1e44088f0a2e8e0c9aa66755573b4c24da5828d77d80bafb0495101dbf6304da3cacd7eaf3a8a11ec8f291a6945a46715f35a9182c5ec55521c`;
--- apps/daimo-mobile/src/view/screen/send/CreateNoteTab.tsx
+++ apps/daimo-mobile/src/view/screen/send/CreateNoteTab.tsx
@@ -27,7 +27,6 @@ import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
 
 import { CancelHeader } from "./CancelHeader";
 import { useSendAsync } from "../../../action/useSendAsync";
-import { fetchNotesContractAllowance } from "../../../logic/note";
 import { useAccount } from "../../../model/account";
 import { ButtonBig } from "../../shared/Button";
 import { AmountInput } from "../../shared/Input";
@@ -37,35 +36,21 @@ import { TextCenter, TextError, TextLight } from "../../shared/text";
 
 /** Creates a Note. User picks amount, then sends message via ShareSheet. */
 export function CreateNoteTab({ hide }: { hide: () => void }) {
-  const [ephemeralPrivateKey, setEphemeralPrivateKey] = useState<Hex>();
+  const [ephemeralPrivKey, setEphemeralPrivKey] = useState<Hex>();
 
   const ephemeralOwner = useMemo(
-    () =>
-      ephemeralPrivateKey
-        ? privateKeyToAccount(ephemeralPrivateKey).address
-        : null,
-    [ephemeralPrivateKey]
+    () => ephemeralPrivKey && privateKeyToAccount(ephemeralPrivKey).address,
+    [ephemeralPrivKey]
   );
 
   const [dollars, setDollars] = useState(0);
 
   const [account] = useAccount();
   assert(account != null);
-  const { address } = account;
 
-  const [isNotesContractApproved, setIsNotesContractApproved] = useState(false);
+  useEffect(() => setEphemeralPrivKey(generatePrivateKey()));
 
-  useEffect(() => {
-    (async () => {
-      // TODO: pull this logic out of the UI.
-      // + automatically approve Notes contract on account creation
-      const allowance = await fetchNotesContractAllowance(address);
-      setIsNotesContractApproved(allowance > 0);
-
-      const privKey = generatePrivateKey();
-      setEphemeralPrivateKey(privKey);
-    })();
-  }, []);
+  const approveFirst = false; // TODO: account.approvedSpenders.includes(ephemeralNotesAddress);
 
   const nonce = new DaimoNonce(new DaimoNonceMetadata());
   const { status, message, exec } = useSendAsync(
@@ -76,7 +61,7 @@ export function CreateNoteTab({ hide }: { hide: () => void }) {
         ephemeralOwner,
         `${dollars}`,
         nonce,
-        !isNotesContractApproved
+        approveFirst
       );
     },
     {
@@ -124,7 +109,7 @@ export function CreateNoteTab({ hide }: { hide: () => void }) {
       const link: DaimoLink = {
         type: "note",
         ephemeralOwner,
-        ephemeralPrivateKey,
+        ephemeralPrivateKey: ephemeralPrivKey,
       };
       const url = formatDaimoLink(link);
 
@@ -149,7 +134,7 @@ export function CreateNoteTab({ hide }: { hide: () => void }) {
     } catch (error: any) {
       console.error("[APP] Note share error:", error);
     }
-  }, [ephemeralOwner, ephemeralPrivateKey, status]);
+  }, [ephemeralOwner, ephemeralPrivKey, status]);
 
   const button = (function () {
     switch (status) {
