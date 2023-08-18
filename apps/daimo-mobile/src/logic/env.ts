c99b47817ed5f1bcd5f04a73ed8a96ae8697fea5
Markus Haas <haas.markus@protonmail.com>
2023-08-18 11:48:24 +0200

    Undo temp api change

--- apps/daimo-mobile/src/logic/env.ts
+++ apps/daimo-mobile/src/logic/env.ts
@@ -1,7 +1,7 @@
 import { nativeApplicationVersion, nativeBuildVersion } from "expo-application";
 
 export const env = {
-  apiUrl: process.env.DAIMO_APP_API_URL || "https://daimo-api-stage.onrender.com",
+  apiUrl: process.env.DAIMO_APP_API_URL || "http://localhost:3000",
   buildProfile: process.env.EAS_BUILD_PROFILE || "local",
   gitHash: (process.env.EAS_BUILD_GIT_COMMIT_HASH || "unknown").substring(0, 8),
   nativeApplicationVersion,
