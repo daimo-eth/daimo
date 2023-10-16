71715f727e3f68fe9cc92f974f3bc3eb39c90e1d
DC <dc@dcpos.ch>
2023-10-16 04:43:35 -0700

    release: 0.5.0 build 54

--- packages/contract/script/script.sh
+++ packages/contract/script/script.sh
@@ -7,14 +7,14 @@ forge build
 # Deploy Account factory
 forge script script/DeployAccountFactory.s.sol --fork-url $RPC_URL --private-key $PRIVATE_KEY --broadcast --verify --etherscan-api-key $ETHERSCAN_API_KEY
 
-# # Deploy name registry
-# forge script script/DeployNameRegistry.s.sol --fork-url $RPC_URL --private-key $PRIVATE_KEY --broadcast --verify  --etherscan-api-key $ETHERSCAN_API_KEY
+# Deploy name registry
+forge script script/DeployNameRegistry.s.sol --fork-url $RPC_URL --private-key $PRIVATE_KEY --broadcast --verify  --etherscan-api-key $ETHERSCAN_API_KEY
 
-# # Deploy Ephemeral Notes using Test USDC
-# forge script script/DeployEphemeralNotes.s.sol --fork-url $RPC_URL --private-key $PRIVATE_KEY --broadcast --verify  --etherscan-api-key $ETHERSCAN_API_KEY
+# Deploy Ephemeral Notes using Test USDC
+forge script script/DeployEphemeralNotes.s.sol --fork-url $RPC_URL --private-key $PRIVATE_KEY --broadcast --verify  --etherscan-api-key $ETHERSCAN_API_KEY
 
 # From DeployAccountFactory
-# ADDR_ACCOUNT_FACTORY="0xe5a1744d42a30c3bb87f16fbc4484b984594cd7c"
+ADDR_ACCOUNT_FACTORY="0xED5F5066de0c177729Eb0D5FECEC4BF87CA6Bf3C"
 
 # Deploy test Account and verify it on Etherscan
-# forge script script/DeployTestAccount.s.sol --sig "run(address)" $ADDR_ACCOUNT_FACTORY --fork-url $RPC_URL --private-key $PRIVATE_KEY --broadcast --verify --etherscan-api-key $ETHERSCAN_API_KEY
+forge script script/DeployTestAccount.s.sol --sig "run(address)" $ADDR_ACCOUNT_FACTORY --fork-url $RPC_URL --private-key $PRIVATE_KEY --broadcast --verify --etherscan-api-key $ETHERSCAN_API_KEY
