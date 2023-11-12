ccaad7063e20b463f16a26144ed7dca9d2016135
Nalin Bhardwaj <nalinbhardwaj@nibnalin.me>
2023-11-12 23:15:35 +0300

    release: 1.3.3 build 78

--- apps/daimo-mobile/app.config.js
+++ apps/daimo-mobile/app.config.js
@@ -1,7 +1,7 @@
 const IS_DEV = process.env.DAIMO_APP_VARIANT === "dev";
 
-const VERSION = "1.3.2";
-const BUILD_NUM = 77;
+const VERSION = "1.3.3";
+const BUILD_NUM = 78;
 
 export default {
   owner: "daimo",
