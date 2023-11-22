a61fb8c8d34f18c2f2793b839bd3d0e34d45cace
Nalin Bhardwaj <nalinbhardwaj@nibnalin.me>
2023-11-22 15:54:33 -0500

    Send: no tab for links

--- apps/daimo-mobile/src/view/screen/send/SendNoteScreen.tsx
+++ apps/daimo-mobile/src/view/screen/send/SendNoteScreen.tsx
@@ -1,11 +1,19 @@
 import { useCallback, useRef, useState } from "react";
-import { TextInput, View } from "react-native";
+import {
+  Keyboard,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 
 import { SendNoteButton } from "./SendNoteButton";
 import { AmountChooser } from "../../shared/AmountInput";
 import { ButtonBig } from "../../shared/Button";
 import { InfoBubble } from "../../shared/InfoBubble";
+import { ScreenHeader, useExitToHome } from "../../shared/ScreenHeader";
 import Spacer from "../../shared/Spacer";
+import { useNav } from "../../shared/nav";
+import { ss } from "../../shared/style";
 import { TextCenter, TextLight } from "../../shared/text";
 
 export function SendNoteScreen() {
@@ -19,36 +27,43 @@ export function SendNoteScreen() {
     setAmountChosen(true);
   }, []);
 
+  const nav = useNav();
+  const goHome = useExitToHome();
+  const goBack = nav.canGoBack() ? nav.goBack : goHome;
+
   return (
-    <View>
-      <Spacer h={8} />
-      <InfoBubble
-        title="Pay by sending a link"
-        subtitle="Anyone with the link can claim it"
-      />
-      <Spacer h={32} />
-      <TextCenter>
-        <TextLight>Enter amount</TextLight>
-      </TextCenter>
-      <Spacer h={24} />
-      <AmountChooser
-        dollars={noteDollars}
-        onSetDollars={setNoteDollars}
-        showAmountAvailable={!amountChosen}
-        autoFocus
-        disabled={amountChosen}
-        innerRef={textInputRef}
-      />
-      <Spacer h={32} />
-      {!amountChosen && (
-        <ButtonBig
-          type="primary"
-          title="Create Payment Link"
-          disabled={!(noteDollars > 0)}
-          onPress={onChooseAmount}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={ss.container.screen}>
+        <ScreenHeader title="Send Link" onBack={goBack} />
+        <Spacer h={8} />
+        <InfoBubble
+          title="Pay by sending a link"
+          subtitle="Anyone with the link can claim it"
         />
-      )}
-      {amountChosen && <SendNoteButton dollars={noteDollars} />}
-    </View>
+        <Spacer h={32} />
+        <TextCenter>
+          <TextLight>Enter amount</TextLight>
+        </TextCenter>
+        <Spacer h={24} />
+        <AmountChooser
+          dollars={noteDollars}
+          onSetDollars={setNoteDollars}
+          showAmountAvailable={!amountChosen}
+          autoFocus
+          disabled={amountChosen}
+          innerRef={textInputRef}
+        />
+        <Spacer h={32} />
+        {!amountChosen && (
+          <ButtonBig
+            type="primary"
+            title="Create Payment Link"
+            disabled={!(noteDollars > 0)}
+            onPress={onChooseAmount}
+          />
+        )}
+        {amountChosen && <SendNoteButton dollars={noteDollars} />}
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
