19167e9c98f57ca12c6236ef8ec8b310c2818272
Nalin <nalinbhardwaj@nibnalin.me>
2023-11-27 23:21:29 -0800

    use react-native-screens for native optimisation

--- apps/daimo-mobile/index.js
+++ apps/daimo-mobile/index.js
@@ -1,10 +1,11 @@
-import { registerRootComponent } from "expo";
-
 // Polyfills for React Native
 import "react-native-get-random-values"; // needs to precede ethers
 
 import "@ethersproject/shims";
 import "text-encoding-polyfill";
+
+import { registerRootComponent } from "expo";
+import { enableScreens } from "react-native-screens";
 
 import App from "./src/App";
 import { initDebugLog } from "./src/debugLog";
@@ -15,5 +16,8 @@ initDebugLog();
 
 // Sync data from chain. Account balance, transfers, ...
 startSync();
+
+// Optimize memory usage and performance with react-native-screens
+enableScreens();
 
 registerRootComponent(App);
