e43314adb9d0c5352395577b508ef86ab703c5bd
DC <dc@dcpos.ch>
2023-12-19 01:00:08 -0800

    release: 1.4.5 build 89

--- apps/daimo-mobile/app.config.js
+++ apps/daimo-mobile/app.config.js
@@ -1,7 +1,7 @@
 const IS_DEV = process.env.DAIMO_APP_VARIANT === "dev";
 
-const VERSION = "1.4.4";
-const BUILD_NUM = 88;
+const VERSION = "1.4.5";
+const BUILD_NUM = 89;
 
 export default {
   owner: "daimo",
