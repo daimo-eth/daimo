44e96134c5e1fe89778e19fa1a00c6af8c2789f6
Nalin <nalinbhardwaj@nibnalin.me>
2023-12-20 05:57:50 +0000

    Cherry pick bug fixes to release (#563)
    * fix web direct deep links
    
    (cherry picked from commit d3dfacb579b73a95c1d30d9eb6e883aa5501a656)
    
    * fix cache timestamp is in seconds, not ms
    
    (cherry picked from commit 4f2dd4d0b3f2b4beb6d14270dc55911912776248)
    
    * remove notev2 stuff
    
    * review comment

--- apps/daimo-web/src/app/link/[[...slug]]/CallToAction.tsx
+++ apps/daimo-web/src/app/link/[[...slug]]/CallToAction.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { DaimoLinkStatus } from "@daimo/common";
+import { DaimoLinkStatus, daimoLinkBase } from "@daimo/common";
+import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 
 import { AppOrWalletCTA } from "./AppOrWalletCTA";
@@ -9,14 +10,21 @@ import { PrimaryOpenInAppButton } from "../../../components/buttons";
 export function CallToAction({
   description,
   walletActionLinkStatus,
-  directDeepLink,
 }: {
   description: string;
   walletActionLinkStatus?: DaimoLinkStatus;
-  directDeepLink: string;
 }) {
   // If we've connected another wallet, hide the Open in Daimo button.
   const { isConnected } = useAccount();
+
+  const [directDeepLink, setDirectDeepLink] = useState<string>("");
+
+  useEffect(() => {
+    // Must be loaded client-side to capture the hash part of the URL
+    // for ephemeral notes.
+    const { href } = window.location;
+    setDirectDeepLink(href.replace(daimoLinkBase, "daimo:/"));
+  }, [directDeepLink]);
 
   return (
     <>
