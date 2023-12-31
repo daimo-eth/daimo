0ca7e3f3bf6e913b866e23cf3200832800f5a445
DC <dc@dcpos.ch>
2023-12-31 12:52:03 -0800

    mobile: remove iPad support

--- apps/daimo-mobile/app.config.js
+++ apps/daimo-mobile/app.config.js
@@ -22,7 +22,6 @@ export default {
     iosDisplayInForeground: true,
   },
   ios: {
-    supportsTablet: true,
     bundleIdentifier: IS_DEV ? "com.daimo.dev" : "com.daimo",
     buildNumber: `${BUILD_NUM}`,
     associatedDomains: [
