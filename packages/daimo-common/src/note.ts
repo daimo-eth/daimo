5356b9abcd6c98fa0146edaec4b08ed2ce099fb2
Nalin <nalinbhardwaj@nibnalin.me>
2024-01-04 04:00:45 -0800

    upgrade to new link format

--- packages/daimo-common/src/note.ts
+++ packages/daimo-common/src/note.ts
@@ -52,13 +52,6 @@ export function generateNoteSeedAddress(): [string, Address] {
   return [seed, noteAddress];
 }
 
-// Remove on link upgrade
-export function getNoteEphPrivKeyFromSeed(seed: string) {
-  const hexSeed = base58.decode(seed);
-  const notePrivateKey = keccak256(hexSeed);
-  return notePrivateKey;
-}
-
-// TODO: remove once EphemeralNote contract no longer requires it.
+// TODO: remove once EphemeralNote V1 contract is banished.
 // Dummy signature of correct length and valid r/s values (so ECDSA.sol doesn't revert)
 const dummySignature: `0x${string}` = `0x001d2a4239a139c1e44088f0a2e8e0c9aa66755573b4c24da5828d77d80bafb0495101dbf6304da3cacd7eaf3a8a11ec8f291a6945a46715f35a9182c5ec55521c`;
