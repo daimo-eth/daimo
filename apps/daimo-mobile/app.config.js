4969875a44227befc8d3d5d3d3b0e2d70121050c
Nalin <nalinbhardwaj@nibnalin.me>
2024-01-21 00:47:39 -0800

    release 1.5.0 build 99

--- apps/daimo-mobile/app.config.js
+++ apps/daimo-mobile/app.config.js
@@ -1,7 +1,7 @@
 const IS_DEV = process.env.DAIMO_APP_VARIANT === "dev";
 
-const VERSION = "1.4.7";
-const BUILD_NUM = 94;
+const VERSION = "1.5.0";
+const BUILD_NUM = 99;
 
 export default {
   owner: "daimo",
