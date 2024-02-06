a7fbd79aeb766e3721a33fc1bcd936b252cd1881
DC <dc@dcpos.ch>
2024-02-06 10:39:57 -0800

    web: redirect /l/download to App Store
    fixes #666

--- apps/daimo-web/src/utils/platform.ts
+++ apps/daimo-web/src/utils/platform.ts
@@ -4,7 +4,7 @@ export function detectPlatform(ua: string): PlatformType {
   // From https://dev.to/konyu/using-javascript-to-determine-whether-the-client-is-ios-or-android-4i1j
   if (/android/i.test(ua)) {
     return "android";
-  } else if (/iPhone|iPod/.test(ua)) {
+  } else if (/iPhone|iPod|iPad/.test(ua)) {
     return "ios";
   }
   return "other";
