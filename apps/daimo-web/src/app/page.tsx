0234b5b91c3f596c4fa4a49425c419b466078451
mikey <michael@pfista.com>
2024-02-21 18:11:12 -0800

    Remove team section

--- apps/daimo-web/src/app/page.tsx
+++ apps/daimo-web/src/app/page.tsx
@@ -19,7 +19,6 @@ export default function HomePage() {
       <SectionWhyDaimo />
       <SectionTestimonial />
       <SectionFAQ faq={parseFAQs(readmeMD)} />
-      <SectionTeam />
       <Footer />
     </>
   );
