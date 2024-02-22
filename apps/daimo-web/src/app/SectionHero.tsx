f8e6f72f1fc11ae977bdea31ab1b89337dcfe7f5
mikey <michael@pfista.com>
2024-02-22 09:26:47 -0800

    Makes hero section mobile responsive

--- apps/daimo-web/src/app/SectionHero.tsx
+++ apps/daimo-web/src/app/SectionHero.tsx
@@ -1,20 +1,22 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import { DownloadLinkButton } from "../components/DownloadLink";
-import { Spacer } from "../components/layout";
+import {
+  DownloadLinkButton,
+  DownloadLinkButtonMobileNav,
+} from "../components/DownloadLink";
 
 export function SectionHero() {
   return (
     <section className="overflow-hidden pb-28">
-      <div className="max-w-screen-xl mx-auto flex flex-col md:flex-row md:gap-16 overflow-visible border rounded-[48px] border-white/30 bg-[#000000]/20 py-16 px-12">
+      <div className="max-w-screen-xl mx-auto flex flex-col md:flex-row md:gap-16 overflow-visible lg:border lg:rounded-[48px] border-white/30 lg:bg-[#000000]/20 py-16 px-12">
         <div className="flex-1 flex flex-col justify-center space-y-8 md:space-y-12">
           <HeroH1>Pay or receive USDC anywhere.</HeroH1>
           <HeroH2>
             Store money using secure hardware on your phone. Yours alone, like
             cash.
           </HeroH2>
-          <div className="flex flex-row items-center space-x-6 pt-6 md:pt-8">
+          <div className="hidden md:flex flex-row items-center space-x-6 pt-6 md:pt-8">
             <DownloadLinkButton />
             <Link className="font-bold text-white text-2xl" href="#faq">
               Learn More<span className="font-extralight">&nbsp;&#8595;</span>
@@ -22,13 +24,18 @@ export function SectionHero() {
           </div>
         </div>
         <div className="flex-1 flex justify-center md:justify-end relative">
-          <Image
-            src="/assets/hero-screenshot.png"
-            width={327} // Consider removing these if you're using Tailwind for responsiveness
-            height={588} // Same as above, control size with Tailwind if possible
-            alt="Screenshots"
-            className="w-full max-w-[200px] md:max-w-sm " // Adjust this for different breakpoints
-          />
+          <div className="mx-auto w-full max-w-[366px] py-6">
+            <Image
+              src="/assets/hero-screenshot.png"
+              width={327}
+              height={588}
+              layout="responsive"
+              alt="Daimo Application Screenshot"
+            />
+          </div>
+        </div>
+        <div className="md:hidden py-6">
+          <DownloadLinkButtonMobileNav />
         </div>
       </div>
     </section>
@@ -37,12 +44,16 @@ export function SectionHero() {
 
 function HeroH1({ children }: { children: React.ReactNode }) {
   return (
-    <h1 className="text-[84px] font-medium text-white leading-[84px] ">
+    <h1 className="text-center md:text-left text-4xl md:text-5xl lg:text-[84px] font-medium text-white lg:leading-[84px] ">
       {children}
     </h1>
   );
 }
 
 function HeroH2({ children }: { children: React.ReactNode }) {
-  return <h1 className="text-[32px] font-medium text-grayLight">{children}</h1>;
+  return (
+    <h1 className="text-center text-[18px] leading-6 tracking-wide md:leading-normal md:tracking-normal md:text-left lg:text-[32px] font-medium text-grayLight">
+      {children}
+    </h1>
+  );
 }
