a7d2c29367ab7570d689c945c20f66a08432bac3
Nalin <nalinbhardwaj@nibnalin.me>
2024-02-27 23:07:30 -0700

    disable NFC on iOS for review (#782)

--- apps/daimo-mobile/src/logic/deeplink.ts
+++ apps/daimo-mobile/src/logic/deeplink.ts
@@ -1,12 +1,11 @@
 import { getInitialURL } from "expo-linking";
-import { Platform } from "react-native";
-import NfcManager, { Ndef, NdefRecord } from "react-native-nfc-manager";
 
 // If an initialURL previously triggered app launch in onboarding state,
 // we don't want to trigger it again.
 let hasHandledInitialInOnboarding = false;
 
 // User opened app via deeplink or scanning an NFC tag: return URL, otherwise null.
+// Currently, tags are disabled in the app, we only use universal deeplinks.
 export async function getInitialURLOrTag(isOnboarding: boolean) {
   if (hasHandledInitialInOnboarding && !isOnboarding) return null;
   if (isOnboarding) hasHandledInitialInOnboarding = true;
@@ -16,34 +15,5 @@ export async function getInitialURLOrTag(isOnboarding: boolean) {
     return deeplinkURL;
   }
 
-  if (Platform.OS === "android") {
-    const tag = await NfcManager.getLaunchTagEvent();
-    if (tag && tag.ndefMessage.length > 0) {
-      console.log(`[DEEPLINK] got NFC tag ${JSON.stringify(tag)}`);
-      try {
-        const message = tag.ndefMessage[0];
-        if (isRecordUint8Array(message.payload) && isURIRecord(message)) {
-          const nfcURL = Ndef.uri.decodePayload(message.payload);
-          console.log(`[DEEPLINK] decoded NFC tag payload: ${nfcURL}`);
-          return nfcURL;
-        } else {
-          console.warn(`[DEEPLINK] NFC tag payload is not a well-formed URI`);
-        }
-      } catch (e) {
-        console.warn(
-          `[DEEPLINK] error decoding NFC tag payload, failing silently: ${e}`
-        );
-      }
-    }
-  }
-
   return null;
-}
-
-function isURIRecord(record: NdefRecord) {
-  return Ndef.isType(record, Ndef.TNF_WELL_KNOWN, Ndef.RTD_URI);
-}
-
-function isRecordUint8Array(payload: unknown): payload is Uint8Array {
-  return true;
 }
