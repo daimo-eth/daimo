5bd0ccc622ed3a6c570115314b17b3db00d2ca30
danielpark <danielpark1140@gmail.com>
2024-03-13 16:25:47 -0700

    Update botResponses.ts

--- apps/daimo-bot/src/botResponses.ts
+++ apps/daimo-bot/src/botResponses.ts
@@ -6,6 +6,7 @@ export const REQUEST_PAYMENT_MESSAGE = (
 ) => {
   return `Here's a request for $${cleanedAmount} to @${recipientUsername}!`;
 };
+
 export const PAYMENT_CONNECT_FC_MESSAGE = (recipientUsername: string) => {
   return `@${recipientUsername} has to first connect their Farcaster on Daimo to receive payments!`;
 };
