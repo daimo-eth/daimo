9808e61624f9a38d931695334be7670bec984182
Wojciech Stanisz <wojciech.stanisz@swmansion.com>
2024-03-13 16:02:43 +0100

    Make web successfull build at any cost ;-;

--- apps/daimo-web/src/utils/rpc.ts
+++ apps/daimo-web/src/utils/rpc.ts
@@ -1,4 +1,4 @@
-import type { AppRouter } from "@daimo/api";
+import { AppRouter } from "@daimo/api";
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 
 import { chainConfig } from "../env";
@@ -6,6 +6,7 @@ import { chainConfig } from "../env";
 const apiUrl = process.env.NEXT_PUBLIC_DAIMO_API_URL || "http://localhost:3000";
 export const apiUrlWithChain = `${apiUrl}/chain/${chainConfig.chainL2.id}`;
 
+// @ts-ignore
 export const rpc = createTRPCProxyClient<AppRouter>({
   links: [httpBatchLink({ url: apiUrlWithChain })],
 });
