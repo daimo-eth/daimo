181267d165e6ac89d49dc8ec74009ad3d60db75c
Oluwakorede Fashokun <koredefashokun@gmail.com>
2024-03-26 19:02:49 -0400

    Set up sheet and screen

---
+++ apps/daimo-mobile/src/view/screen/SeedPhraseScreen.tsx
@@ -0,0 +1,16 @@
+import { View } from "react-native";
+import Animated from "react-native-reanimated";
+
+export function SeedPhraseScreen() {
+  return <View />;
+}
+
+export function ProgressBlobs({
+  activeStep,
+  steps,
+}: {
+  activeStep: number;
+  steps: number;
+}) {
+  return <Animated.View />;
+}
