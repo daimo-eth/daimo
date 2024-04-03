e2045140bce7c2dccf248dc30411f85a2055c9d5
Oluwakorede Fashokun <koredefashokun@gmail.com>
2024-04-03 18:08:15 -0400

    It works

--- apps/daimo-mobile/src/logic/seedPhrase.ts
+++ apps/daimo-mobile/src/logic/seedPhrase.ts
@@ -1,15 +1,33 @@
+import { contractFriendlyKeyToDER } from "@daimo/common";
 import { p256 } from "@noble/curves/p256";
 import * as bip39 from "@scure/bip39";
-import { bytesToHex } from "viem";
+import { ec } from "elliptic";
+import { Hex, bytesToHex } from "viem";
 
 import { wordlist } from "./wordlist";
 
+const p256Curve = new ec("p256");
+
 export function generateSeedPhrase() {
   const entropy = p256.utils.randomPrivateKey();
-  const mnemonic = bip39.entropyToMnemonic(entropy, wordlist);
   const publicKey = bytesToHex(p256.getPublicKey(entropy));
+  const mnemonic = bip39.entropyToMnemonic(entropy, wordlist);
 
-  return { mnemonic, publicKey };
+  const [x, y] = getPublicKeyCoordinates(publicKey);
+  const publicKeyDER = contractFriendlyKeyToDER([x, y]);
+
+  return { mnemonic, publicKey: publicKeyDER };
+}
+
+function getPublicKeyCoordinates(publicKey: Hex): [Hex, Hex] {
+  const publicKeyPoint = p256Curve
+    .keyFromPublic(publicKey.slice(2), "hex")
+    .getPublic();
+
+  const x = `0x${publicKeyPoint.getX().toString(16)}` as Hex;
+  const y = `0x${publicKeyPoint.getY().toString(16)}` as Hex;
+
+  return [x, y];
 }
 
 export function reverseSeedPhrase(mnemonic: string) {
