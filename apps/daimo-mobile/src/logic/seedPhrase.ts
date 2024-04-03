3c7fbfd8b6a18d6ecdc0fbbd78b1025f3d063aab
Oluwakorede Fashokun <koredefashokun@gmail.com>
2024-04-03 05:51:58 -0400

    Add new wordlist

--- apps/daimo-mobile/src/logic/seedPhrase.ts
+++ apps/daimo-mobile/src/logic/seedPhrase.ts
@@ -1,7 +1,8 @@
 import { p256 } from "@noble/curves/p256";
 import * as bip39 from "@scure/bip39";
-import { wordlist } from "@scure/bip39/wordlists/english";
 import { bytesToHex } from "viem";
+
+import { wordlist } from "./wordlist";
 
 export function generateSeedPhrase() {
   const entropy = p256.utils.randomPrivateKey();
