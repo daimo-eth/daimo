5b34df74742ae37c28c34a00ceb3158de24c22c5
Oluwakorede Fashokun <koredefashokun@gmail.com>
2024-04-03 23:05:09 -0400

    Fix lint issue

--- apps/daimo-mobile/src/view/sheet/OwnRequestBottomSheet.tsx
+++ apps/daimo-mobile/src/view/sheet/OwnRequestBottomSheet.tsx
@@ -63,6 +63,7 @@ export function OwnRequestBottomSheet({
           .concat([updatedRequest]),
       };
     },
+    signerType: "deviceKey",
   });
 
   useEffect(() => {
--- apps/daimo-mobile/test/seedPhrase.test.ts
+++ apps/daimo-mobile/test/seedPhrase.test.ts
@@ -1,11 +1,19 @@
 import { isDERPubKey } from "@daimo/common";
 
-import { generateSeedPhrase } from "../src/logic/seedPhrase";
+import { generateSeedPhrase, reverseSeedPhrase } from "../src/logic/seedPhrase";
 
 describe("Seed Phrase", () => {
+  const { mnemonic, publicKey } = generateSeedPhrase();
+
   it("generates a mnemonic and a public key", () => {
-    const { mnemonic, publicKey } = generateSeedPhrase();
     expect(mnemonic.split(" ").length).toEqual(24);
     expect(isDERPubKey(publicKey)).toEqual(true);
+  });
+
+  it("correctly reverses a mnemonic", () => {
+    const privateKey = reverseSeedPhrase(mnemonic);
+
+    // TODO: Test that the retrieved *publicKey* matches the one above.
+    expect(privateKey).toBeDefined();
   });
 });
