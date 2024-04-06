e7b20dc61c3460d941e0cf36eaa69d12ba737ad3
Oluwakorede Fashokun <koredefashokun@gmail.com>
2024-04-05 22:34:39 -0400

    Add dynamic text

--- apps/daimo-web/src/components/image-gen/InvitePreview.tsx
+++ apps/daimo-web/src/components/image-gen/InvitePreview.tsx
@@ -8,28 +8,44 @@ export function InvitePreviewImg({ name }: { name: string }) {
         height: "100%",
         width: "100%",
         display: "flex",
-        justifyContent: "center",
+        alignItems: "center",
         flexDirection: "column",
       }}
     >
       <div
         style={{
           backgroundImage: `url("${getAbsoluteUrl(
-            "/assets/preview-pixel-bg.svg"
+            "/assets/frame/InvitePowerUserDynamic.png"
           )}")`,
           height: "100%",
           width: "100%",
           display: "flex",
-          justifyContent: "space-between",
-          flexDirection: "column",
+          alignItems: "center",
+          paddingTop: 74,
         }}
       >
-        <Content />
+        <Content name={name} />
       </div>
     </div>
   );
 }
 
-function Content() {
-  return <div />;
+function Content({ name }: { name: string }) {
+  return (
+    <div>
+      <div
+        style={{
+          color: "#8b5af6",
+          fontSize: 18,
+          marginBottom: 24,
+          textAlign: "center",
+        }}
+      >
+        +$10 BONUS FOR POWER USERS
+      </div>
+      <div style={{ fontSize: 36, textAlign: "center" }}>
+        Join {name} on <span>Daimo.</span>
+      </div>
+    </div>
+  );
 }
