9ae8fa7ba2ae500670d02debbdcd9273e2ad0e7e
Oluwakorede Fashokun <koredefashokun@gmail.com>
2024-04-13 14:39:27 -0400

    Fix @trpc/client issue

--- apps/daimo-clippy/src/utils/rpc.ts
+++ apps/daimo-clippy/src/utils/rpc.ts
@@ -3,7 +3,7 @@ import { createTRPCClient, httpBatchLink } from "@trpc/client";
 
 import { chainConfig } from "../env";
 
-const apiUrl = process.env.NEXT_PUBLIC_DAIMO_API_URL || "http://localhost:3000";
+const apiUrl = process.env.DAIMO_API_URL || "http://localhost:3000";
 export const apiUrlWithChain = `${apiUrl}/chain/${chainConfig.chainL2.id}`;
 
 export const rpc = createTRPCClient<AppRouter>({
