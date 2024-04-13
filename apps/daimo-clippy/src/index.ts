e0d2ef1935116248a212aa4f32566cded2d0f68a
Oluwakorede Fashokun <koredefashokun@gmail.com>
2024-04-13 08:11:41 -0400

    Body parser extended false

--- apps/daimo-clippy/src/index.ts
+++ apps/daimo-clippy/src/index.ts
@@ -1,6 +1,7 @@
-import express from "express";
 import * as bodyParser from "body-parser";
 import * as dotenv from "dotenv";
+import express from "express";
+
 import webhookRouter from "./webhook";
 
 dotenv.config();
@@ -9,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 4100;
 
 app.use(express.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/webhook", webhookRouter);
 
