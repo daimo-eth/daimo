bb21bc5b192026c49e2343f4e3a4012332553677
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2024-04-18 16:46:52 -0700

    add types

--- packages/daimo-api/src/api/getMemo.ts
+++ packages/daimo-api/src/api/getMemo.ts
@@ -3,18 +3,31 @@ import { Hex } from "viem";
 import { OpIndexer, UserOp } from "../contract/opIndexer";
 import { PaymentMemoTracker } from "../offchain/paymentMemoTracker";
 
+type TransactionMemo = {
+  txHash: Hex;
+  logIndex: number;
+  opHash?: Hex;
+  memo?: string;
+};
+
 /** Retreives a payment memo given a transaction hash and log index. */
 export async function getMemo(
   txHash: string,
   logIndex: number,
   opIndexer: OpIndexer,
   paymentMemoTracker: PaymentMemoTracker
-): Promise<string | undefined> {
+): Promise<TransactionMemo | undefined> {
   const userOp: UserOp | undefined = opIndexer.fetchUserOpLog(
     txHash as Hex,
     logIndex
   );
   const opHash = userOp?.hash;
   const memo = opHash ? paymentMemoTracker.getMemo(opHash) : undefined;
-  return memo;
+  const transactionMemo: TransactionMemo = {
+    txHash: txHash as Hex,
+    logIndex,
+    opHash,
+    memo,
+  };
+  return transactionMemo;
 }
