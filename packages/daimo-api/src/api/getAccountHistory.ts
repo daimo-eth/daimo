7ba75182ab3d3642202ceb39967d90923ac65b82
Nalin Bhardwaj <github@nibnalin.me>
2024-06-18 13:24:46 -0700

    upgrade relay bridge link

--- packages/daimo-api/src/api/getAccountHistory.ts
+++ packages/daimo-api/src/api/getAccountHistory.ts
@@ -320,7 +320,7 @@ function getRampNetworkURL(account: EAccount) {
 }
 
 function getBridgeURL(account: EAccount) {
-  return `https://www.relay.link/bridge/base/?currency=usdc&toAddress=${account.addr}&lockToChain=true&lockCurrency=true&header=daimo`;
+  return `https://www.relay.link/daimo?toChainId=8453&toCurrency=0x833589fcd6edb6e08f4c7c32d4f71b54bda02913&lockToToken=true&toAddress=${account.addr}`;
 }
 
 function getCoinbaseURL(account: EAccount) {
