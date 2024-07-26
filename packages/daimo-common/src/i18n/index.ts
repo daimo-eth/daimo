4828626a6ae6947592bb6bdb30028abe4ce17978
Kaylee George <kay.r.george@gmail.com>
2024-07-26 10:32:01 -0700

    wip

---
+++ packages/daimo-common/src/i18n/index.ts
@@ -0,0 +1,31 @@
+import { getLocales, Locale } from "expo-localization";
+
+import { en } from "./languages/en";
+
+type I18NTranslation = typeof en;
+
+export function getI18NLocale(): Locale {
+  return getLocales()[0];
+}
+
+export const i18n: I18NTranslation = (function () {
+  // Get current system locale.
+  const { languageCode } = getI18NLocale();
+
+  switch (languageCode) {
+    // TODO: handle other languages here
+    // case "es":
+    default:
+      return en;
+  }
+})();
+
+export function localeToLanguage(locale: Locale) {
+  switch (locale.languageCode) {
+    case "es":
+      return "Español";
+    case "en":
+    default:
+      return "English";
+  }
+}
