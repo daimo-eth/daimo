b396dff4fd9c32b78d870ff1bdd2b985f20a2249
ethan <contact@ethan-rouimi.com>
2024-08-04 12:30:59 +0100

    eliminate unsued code

--- apps/daimo-web/src/i18n/languages/en.ts
+++ apps/daimo-web/src/i18n/languages/en.ts
@@ -346,9 +346,4 @@ export const en = {
   },
 };
 
-function pluralize(n: number, noun: string) {
-  if (n === 1) return `${n} ${noun}`; // "1 apple"
-  return `${n} ${noun}s`; // "0 apples" or "2 apples"
-}
-
 export type LanguageDefinition = typeof en;
--- apps/daimo-web/src/i18n/languages/es.ts
+++ apps/daimo-web/src/i18n/languages/es.ts
@@ -344,13 +344,3 @@ export const es: LanguageDefinition = {
     },
   },
 };
-
-function pluralize(n: number, noun: string) {
-  if (n === 1) return `${n} ${noun}`; // "1 manzana"
-
-  if (noun.slice(-3) === "ión") {
-    return `${n} ${noun.slice(0, -3)}iones`; // "2 canciones" o "{n} canciones"
-  }
-
-  return `${n} ${noun}s`; // "2 manzanas" o "{n} manzanas"
-}
