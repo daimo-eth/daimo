df2349a36c3206bc826510817a053cd1f7f403c9
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2024-08-09 23:01:27 -0300

    api: fetch blue dollar exchange rates (#1249)
    * fetch blue dollar ars
    
    * add bolivian

--- packages/daimo-api/src/api/getExchangeRates.ts
+++ packages/daimo-api/src/api/getExchangeRates.ts
@@ -41,7 +41,7 @@ export async function getExchangeRatesInner(extApiCache: ExternalApiCache) {
 }
 
 async function fetchExchangeRates() {
-  // Fetch JSON from EXCHANGE_RATES_URL using fetch()
+  // Fetch JSON from EXCHANGE_RATES_URL using fetch() for non-USD currencies
   const ratesUrl = getEnvApi().EXCHANGE_RATES_URL;
   console.log(`[API] fetching exchange rates from ${ratesUrl}`);
   const res = await fetchWithBackoff(new URL(ratesUrl));
@@ -49,7 +49,30 @@ async function fetchExchangeRates() {
     throw new Error(`Failed to fetch exchange rates: ${res.statusText}`);
   }
   const retObj = await res.json();
+
+  // Replace ARS and BOB with Blue Dollar rate if available
+  const arsUSD = getLatamRate("usdt/ars");
+  const bobUSD = getLatamRate("usdt/bob");
+  if (arsUSD) retObj.rates["ARS"] = arsUSD;
+  if (bobUSD) retObj.rates["BOB"] = bobUSD;
+
   const retStr = JSON.stringify(retObj);
   console.log(`[API] got currency exchange rates: ${retStr}`);
   return retStr;
+}
+
+async function getLatamRate(pairPath: string) {
+  const LATIN_AMERICA_EXCHANGE_RATE_URL = "https://criptoya.com/api/";
+  const url = LATIN_AMERICA_EXCHANGE_RATE_URL + pairPath;
+  console.log(`[API] fetching blue dollar rate for ${pairPath} from ${url}`);
+  const res = await fetchWithBackoff(new URL(url));
+  if (!res.ok) {
+    console.log(`Failed to fetch Latam exchange rates: ${res}`);
+    return null;
+  }
+  const rateObj = await res.json();
+  if (rateObj.binancep2p && rateObj.binancep2p.ask > 0) {
+    const midMarketRate = (rateObj.binancep2p.ask + rateObj.binancep2p.bid) / 2;
+    return midMarketRate;
+  }
 }
