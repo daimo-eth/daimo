214aaa3caf14d3e9e9f7f1def62d4d83d01a50ba
Kaylee George <kay.r.george@gmail.com>
2024-08-09 22:20:26 -0300

    remove route description

--- apps/daimo-mobile/src/common/featureFlag.ts
+++
@@ -1,7 +0,0 @@
-import { Account } from "../storage/account";
-
-export class FeatFlag {
-  public static sendAnyCoin(account: Account) {
-    return ["dcposch", "klee", "andrewliu"].includes(account.name);
-  }
-}
