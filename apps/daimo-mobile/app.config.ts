8278c45f0c0e500ef2ded18e8ff1de1bc5f1d051
DC <dc@dcpos.ch>
2024-08-13 16:48:22 -0300

    release: 1.9.4

--- apps/daimo-mobile/app.config.ts
+++ apps/daimo-mobile/app.config.ts
@@ -2,7 +2,7 @@ import type { ExpoConfig } from "@expo/config";
 
 const IS_DEV = process.env.DAIMO_APP_VARIANT === "dev";
 
-const VERSION = "1.9.4";
+const VERSION = "1.9.5";
 
 const config: ExpoConfig = {
   owner: "daimo",
