e49924527a2e09d0d2f801a02609cbecc6d7e705
Andrew <andrewliu08@gmail.com>
2024-08-30 23:01:56 -0700

    api: better logging and error handling for landline

--- packages/daimo-api/src/landline/connector.ts
+++ packages/daimo-api/src/landline/connector.ts
@@ -35,22 +35,46 @@ export function getLandlineURL(daimoAddress: string, sessionKey: string) {
 export async function getLandlineSession(
   daimoAddress: Address
 ): Promise<LandlineSessionKey> {
-  // @ts-ignore
-  const sessionKey = await landlineTrpc.getOrCreateSessionKey.mutate({
-    daimoAddress,
-  });
-  return sessionKey;
+  console.log(`[LANDLINE] getting session key for ${daimoAddress}`);
+
+  try {
+    // @ts-ignore
+    const sessionKey = await landlineTrpc.getOrCreateSessionKey.mutate({
+      daimoAddress,
+    });
+    console.log(`[LANDLINE] got session key for ${daimoAddress}`);
+    return sessionKey;
+  } catch (err: any) {
+    console.error(
+      `[LANDLINE] error getting session key for ${daimoAddress}`,
+      err
+    );
+    // Gracefully return empty string
+    return { key: "" };
+  }
 }
 
 export async function getLandlineAccounts(
   daimoAddress: Address
 ): Promise<LandlineAccount[]> {
-  const landlineAccounts =
-    // @ts-ignore
-    await landlineTrpc.getExternalAccountsTransferInfo.query({
-      daimoAddress,
-    });
-  return landlineAccounts;
+  console.log(`[LANDLINE] getting external accounts for ${daimoAddress}`);
+
+  try {
+    const landlineAccounts =
+      // @ts-ignore
+      await landlineTrpc.getExternalAccountsTransferInfo.query({
+        daimoAddress,
+      });
+    console.log(`[LANDLINE] got external accounts for ${daimoAddress}`);
+    return landlineAccounts;
+  } catch (err: any) {
+    console.error(
+      `[LANDLINE] error getting external accounts for ${daimoAddress}`,
+      err
+    );
+    // Gracefully return empty array
+    return [];
+  }
 }
 
 export async function landlineDeposit(
@@ -65,11 +89,23 @@ export async function landlineDeposit(
     amount,
     memo,
   });
-  // @ts-ignore
-  return await landlineTrpc.deposit.mutate({
-    daimoAddress,
-    landlineAccountUuid,
-    amount,
-    memo,
-  });
+
+  try {
+    // @ts-ignore
+    const depositResponse = await landlineTrpc.deposit.mutate({
+      daimoAddress,
+      landlineAccountUuid,
+      amount,
+      memo,
+    });
+    console.log(
+      `[LANDLINE] created deposit for ${daimoAddress}, landlineAccountUuid: ${landlineAccountUuid}, amount: ${amount}, memo: ${memo}`,
+      depositResponse
+    );
+    return depositResponse;
+  } catch (err: any) {
+    console.error(`[LANDLINE] error making deposit`, err);
+    // Gracefully return error status
+    return { status: "error" };
+  }
 }
