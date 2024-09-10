de05ac160f0f13b6c43c0c16ecbb299a7ae37ce3
Kaylee George <kay.r.george@gmail.com>
2024-09-09 19:05:42 -0700

    api: add to feat flag

--- packages/daimo-api/src/api/featureFlag.ts
+++ packages/daimo-api/src/api/featureFlag.ts
@@ -2,8 +2,13 @@ import { EAccount } from "@daimo/common";
 
 export class FeatFlag {
   public static landline(account: EAccount & { name: string }) {
-    return ["dcposch", "klee", "andrewliu", "nibnalin", "hanna"].includes(
-      account.name
-    );
+    return [
+      "dcposch",
+      "klee",
+      "andrewliu",
+      "nibnalin",
+      "hanna",
+      "sofiane",
+    ].includes(account.name);
   }
 }
