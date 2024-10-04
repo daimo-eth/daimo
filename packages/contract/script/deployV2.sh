c47092f0069b378afbd3ba356fc5677fba09f519
DC <dc@dcpos.ch>
2024-10-04 10:36:08 -0700

    contract: fix PayIntent

--- packages/contract/script/deployV2.sh
+++ packages/contract/script/deployV2.sh
@@ -11,8 +11,8 @@ set -e
 # ETHERSCAN_API_KEY_... for each target chain
 
 SCRIPTS=(
-    "script/DeployCrepeHandoffFactory.s.sol"
-    # "script/DeployCrepeFastCCTP.s.sol"
+    # "script/DeployCrepeHandoffFactory.s.sol"
+    "script/DeployCrepeFastCCTP.s.sol"
     # "script/DeployFlexSwapper.s.sol"
     # "script/DeployCCTPBridger.s.sol"
     # "script/DeployAccountFactoryV2.s.sol"
@@ -24,10 +24,10 @@ SCRIPTS=(
 )
 CHAINS=(
     # MAINNETS
-    # "$ETHERSCAN_API_KEY_BASE,https://base-mainnet.g.alchemy.com/v2/$ALCHEMY_API_KEY"
-    # "$ETHERSCAN_API_KEY_OP,https://opt-mainnet.g.alchemy.com/v2/$ALCHEMY_API_KEY"
-    # "$ETHERSCAN_API_KEY_ARB,https://arb-mainnet.g.alchemy.com/v2/$ALCHEMY_API_KEY"
-    # "$ETHERSCAN_API_KEY_POLYGON,https://polygon-mainnet.g.alchemy.com/v2/$ALCHEMY_API_KEY"
+    "$ETHERSCAN_API_KEY_BASE,https://base-mainnet.g.alchemy.com/v2/$ALCHEMY_API_KEY"
+    "$ETHERSCAN_API_KEY_OP,https://opt-mainnet.g.alchemy.com/v2/$ALCHEMY_API_KEY"
+    "$ETHERSCAN_API_KEY_ARB,https://arb-mainnet.g.alchemy.com/v2/$ALCHEMY_API_KEY"
+    "$ETHERSCAN_API_KEY_POLYGON,https://polygon-mainnet.g.alchemy.com/v2/$ALCHEMY_API_KEY"
     # "$ETHERSCAN_API_KEY_L1,https://eth-mainnet.g.alchemy.com/v2/$ALCHEMY_API_KEY" # Expensive, deploy last
 
     # Not supporting Avalanche, skip.
@@ -35,7 +35,7 @@ CHAINS=(
     # "verifyContract,https://avalanche-c-chain-rpc.publicnode.com"  
 
     # TESTNETS
-    "$ETHERSCAN_API_KEY_L1,wss://ethereum-sepolia-rpc.publicnode.com"
+    # "$ETHERSCAN_API_KEY_L1,wss://ethereum-sepolia-rpc.publicnode.com"
     # "$ETHERSCAN_API_KEY_BASE,https://sepolia.base.org"
  
 )
