94d83388943fd39d471914ccf458ed8ce2ab4255
Andrew <andrewliu08@gmail.com>
2024-10-05 10:41:08 -0700

    pay contract: new bridging contracts

---
+++ packages/contract/src/pay/DaimoPayCCTPBridger.sol
@@ -0,0 +1,147 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.12;
+
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import "openzeppelin-contracts-upgradeable/contracts/access/Ownable2StepUpgradeable.sol";
+import "openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
+
+import "../interfaces/IDaimoPayBridger.sol";
+import "../../vendor/cctp/ITokenMinter.sol";
+import "../../vendor/cctp/ICCTPTokenMessenger.sol";
+
+/// @title Bridger implementation for Circle's Cross-Chain Transfer Protocol (CCTP)
+/// @author The Daimo team
+/// @custom:security-contact security@daimo.com
+///
+/// Bridges assets from to a destination chain using CCTP. The only supported
+/// bridge token is USDC.
+contract DaimoPayCCTPBridger is IDaimoPayBridger {
+    using SafeERC20 for IERC20;
+
+    address public immutable owner;
+
+    // CCTP TokenMinter for this chain. Used to identify the CCTP token on the
+    // current chain.
+    ITokenMinter public immutable tokenMinter;
+    // CCTP TokenMessenger for this chain. Used to initiate the CCTP bridge.
+    ICCTPTokenMessenger public immutable cctpMessenger;
+
+    // Map chainId to CCTP domain. CCTP uses 0 as a domain. In order to use
+    // 0 as a not-found value, store CCTP domain + 1 in the mapping.
+    // 0 = not found, 1 = Ethereum, 2 = Avalanche, etc.
+    mapping(uint256 chainId => uint32 domain) public cctpDomainMapping;
+
+    event DomainAdded(uint256 indexed chainId, uint32 domain);
+
+    /// Specify owner (not msg.sender) to allow CREATE3 deployment.
+    constructor(
+        address _initialOwner,
+        ITokenMinter _tokenMinter,
+        ICCTPTokenMessenger _cctpMessenger
+    ) {
+        owner = _initialOwner;
+        tokenMinter = _tokenMinter;
+        cctpMessenger = _cctpMessenger;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "DPCCTPB: caller is not the owner");
+        _;
+    }
+
+    // ----- ADMIN FUNCTIONS -----
+
+    /// Initialize. Specify the CCTP chain IDs and domains that this bridger
+    /// will support.
+    function init(
+        uint256[] memory _cctpChainIds,
+        uint32[] memory _cctpDomains
+    ) public onlyOwner {
+        uint256 n = _cctpChainIds.length;
+        require(n == _cctpDomains.length, "DPCCTPB: wrong cctpDomains length");
+
+        for (uint256 i = 0; i < n; ++i) {
+            _addDomain(_cctpChainIds[i], _cctpDomains[i]);
+        }
+    }
+
+    /// Add a new supported CCTP recipient chain.
+    function addCCTPDomain(uint256 chainId, uint32 domain) public onlyOwner {
+        _addDomain(chainId, domain);
+    }
+
+    function _addDomain(uint256 chainId, uint32 domain) private {
+        require(chainId != 0, "DPCCTPB: missing chainId");
+        // CCTP uses 0 as a domain. In order to use 0 as a not-found value,
+        // store CCTP domain + 1 in the mapping.
+        cctpDomainMapping[chainId] = domain + 1;
+        emit DomainAdded(chainId, domain);
+    }
+
+    function _getDomain(uint256 chainId) internal view returns (uint32) {
+        uint32 domain = cctpDomainMapping[chainId];
+        // The mapping stores CCTP domain + 1 and reserves 0 for not-found.
+        require(domain != 0, "DPCCTPB: missing domain");
+        return domain - 1;
+    }
+
+    function addressToBytes32(address addr) internal pure returns (bytes32) {
+        return bytes32(uint256(uint160(addr)));
+    }
+
+    // ----- PUBLIC FUNCTIONS -----
+
+    /// Get the CCTP token for the current chain corresponding to the
+    /// destination chain's CCTP token.
+    function getCurrentChainCCTPToken(
+        uint32 toDomain,
+        address toToken
+    ) public view returns (IERC20) {
+        return
+            IERC20(
+                tokenMinter.getLocalToken(toDomain, addressToBytes32(toToken))
+            );
+    }
+
+    /// Initiate a bridge to a destination chain using CCTP.
+    function sendToChain(
+        uint256 toChainId,
+        address toAddress,
+        address toToken,
+        uint256 toAmount,
+        bytes calldata /* extraData */
+    ) public {
+        require(toChainId != block.chainid, "DPCCTPB: same chain");
+        require(toAmount > 0, "DPCCTPB: zero amount");
+
+        uint32 toDomain = _getDomain(toChainId);
+        IERC20 fromToken = getCurrentChainCCTPToken(toDomain, toToken);
+
+        // Move input token from caller to this contract and approve CCTP.
+        IERC20(fromToken).safeTransferFrom({
+            from: msg.sender,
+            to: address(this),
+            value: toAmount
+        });
+        IERC20(fromToken).forceApprove({
+            spender: address(cctpMessenger),
+            value: toAmount
+        });
+
+        cctpMessenger.depositForBurn({
+            amount: toAmount,
+            destinationDomain: toDomain,
+            mintRecipient: addressToBytes32(toAddress),
+            burnToken: address(fromToken)
+        });
+
+        emit BridgeInitiated(
+            msg.sender,
+            toChainId,
+            toAddress,
+            toToken,
+            toAmount
+        );
+    }
+}
--- packages/contract/src/pay/PayIntent.sol
+++ packages/contract/src/pay/PayIntent.sol
@@ -4,117 +4,123 @@ pragma solidity ^0.8.12;
 import "openzeppelin-contracts/contracts/proxy/utils/Initializable.sol";
 import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import "../vendor/cctp/ICCTPTokenMessenger.sol";
-import "./CrepeUtils.sol";
+import "./TokenUtils.sol";
+import "../interfaces/IDaimoPayBridger.sol";
 
-// This contract is deployed as an ERC1967Proxy to save gas, then destroyed
-// again in the same transaction.
-// CCTP lets us pass just a few  pieces of information from chain A to chain B:
-// sender, recipient, token, amount. This contract lets us encode all of the
-// FastCCTP send parameters into the sender address via CREATE2.
-contract CrepeHandoff is Initializable {
+/// @dev Asset amount, e.g. $100 USDC or 0.1 ETH
+struct TokenAmount {
+    /// @dev Zero address = native asset, e.g. ETH
+    IERC20 addr;
+    uint256 amount;
+}
+
+/// @dev Represents a destination address + optional arbitrary contract call
+struct Call {
+    /// @dev Destination receiving address or contract
+    address to;
+    /// @dev Native token amount for call, or 0
+    uint256 value;
+    /// @dev Calldata for call, or empty = no contract call
+    bytes data;
+}
+
+/// @dev Represents an intended call: "make X of token Y show up on chain Z, then
+///      use it to do an arbitrary contract call".
+struct PayIntent {
+    /// @dev Intent only executes on given target chain.
+    uint256 chainId;
+    /// @dev Expected token after bridging to the destination chain.
+    TokenAmount bridgeToken;
+    /// @dev Expected token amount after swapping on the destination chain.
+    TokenAmount finalCallToken;
+    /// @dev Destination on target chain. If dest.data != "" specifies a call,
+    ///     (token, amount) is approved. Otherwise, it's transferred to dest.to
+    Call finalCall;
+    /// @dev Escrow contract for fast-finish.
+    address payable escrow;
+    /// @dev Address to refund tokens if call fails, or zero.
+    address refundAddress;
+    /// @dev Nonce. PayIntent receiving addresses are one-time use.
+    uint256 nonce;
+}
+
+IDaimoPayBridger constant bridger = IDaimoPayBridger(address(0)); // TODO
+
+/// @dev This is an ephemeral intent contract. Any supported tokens sent to this
+///      address on any supported chain are forwarded, via a combination of
+///      bridging and swapping, into a specified call on a destination chain.
+contract PayIntentContract is Initializable {
     using SafeERC20 for IERC20;
 
-    address payable private creator;
-    Destination private destination;
+    /// @dev Save gas by minimizing storage to a single word. This makes intents
+    ///      usable on L1. intentHash = keccak(abi.encode(PayIntent))
+    bytes32 intentHash;
 
-    /// Runs at deploy time. Singleton implementation contract = no init, no
-    /// state. All other methods are called via proxy = initialized once, has
-    /// state.
+    /// @dev Runs at deploy time. Singleton implementation contract = no init,
+    ///      no state. All other methods are called via proxy.
     constructor() {
         _disableInitializers();
     }
 
-    function initialize(
-        address payable _creator,
-        Destination calldata _destination
-    ) public initializer {
-        creator = _creator;
-        destination = _destination;
+    function initialize(bytes32 _intentHash) public initializer {
+        intentHash = _intentHash;
     }
 
-    function addressToBytes32(address addr) internal pure returns (bytes32) {
-        return bytes32(uint256(uint160(addr)));
-    }
-
-    // Executes two steps: swap tokenIn to expectedBurnToken, and then burn it
-    // if we are not on the destination chain.
+    /// Called on the source chain to initiate the intent. Sends funds to dest
+    /// chain.
     function sendAndSelfDestruct(
-        ICCTPTokenMessenger cctpMessenger,
-        TokenAmount[] calldata approvals,
-        Call calldata swapCall,
-        TokenAmount calldata expectedBurnToken
+        PayIntent calldata intent,
+        Call[] calldata swapCalls,
+        bytes calldata bridgeExtraData
     ) public {
-        require(msg.sender == creator, "FCCTP: only creator");
+        require(keccak256(abi.encode(intent)) == intentHash, "PI: intent");
+        require(msg.sender == intent.escrow, "PI: only escrow");
+        require(block.chainid != intent.chainId, "PI: only foreign chain");
 
-        if (swapCall.data.length > 0) {
-            // Approve and swap any user inputs as required.
-            for (uint256 i = 0; i < approvals.length; i++) {
-                CrepeTokenUtils.approve(
-                    approvals[i].addr,
-                    address(swapCall.to),
-                    approvals[i].amount
-                );
-            }
-
-            (bool success, ) = swapCall.to.call{value: swapCall.value}(
-                swapCall.data
-            );
-            require(success, "FCCTP: swap call failed");
+        // Run arbitrary calls provided by the LP. These will generally swap if
+        // necessary, then approve tokens to the bridger.
+        for (uint256 i = 0; i < swapCalls.length; ++i) {
+            Call calldata call = swapCalls[i];
+            (bool success, ) = call.to.call{value: call.value}(call.data);
+            require(success, "PI: swap call failed");
         }
 
-        // Redirect bridge token to FastCCTP to burn.
-        uint256 burnBalance = CrepeTokenUtils.getBalanceOf(
-            expectedBurnToken.addr,
-            address(this)
-        );
+        bridger.sendToChain({
+            toChainId: intent.chainId,
+            toAddress: address(this),
+            toToken: address(intent.bridgeToken.addr),
+            toAmount: intent.bridgeToken.amount,
+            extraData: bridgeExtraData
+        });
+
+        // This use of SELFDESTRUCT is compatible with EIP-6780. Ephemeral
+        // contracts are deployed, then destroyed in the same transaction.
+        // solhint-disable-next-line
+        selfdestruct(intent.escrow);
+    }
+
+    /// One step: receive mintToken and send to creator
+    function receiveAndSelfDestruct(PayIntent calldata intent) public {
+        require(keccak256(abi.encode(intent)) == intentHash, "PI: intent");
+        require(msg.sender == intent.escrow, "PI: only creator");
+        require(block.chainid == intent.chainId, "PI: only dest chain");
+
+        IERC20 bridgeTok = intent.bridgeToken.addr;
+        uint256 amount = TokenUtils.getBalanceOf(bridgeTok, address(this));
         require(
-            burnBalance >= expectedBurnToken.amount,
-            "FCCTP: insufficient burn token"
+            amount >= intent.bridgeToken.amount,
+            "PI: insufficient bridge token received"
         );
 
-        if (destination.chainId != block.chainid) {
-            // Burn to CCTP. Recipient = EphemeralHandoff address on chain B.
-            CrepeTokenUtils.approve(
-                expectedBurnToken.addr,
-                address(cctpMessenger),
-                expectedBurnToken.amount
-            );
-            cctpMessenger.depositForBurn({
-                amount: expectedBurnToken.amount,
-                destinationDomain: destination.domain,
-                mintRecipient: addressToBytes32(address(this)),
-                burnToken: address(expectedBurnToken.addr)
-            });
-        }
+        // Send to escrow contract, which will forward to current recipient
+        TokenUtils.transfer(intent.bridgeToken.addr, intent.escrow, amount);
 
         // This use of SELFDESTRUCT is compatible with EIP-6780. Handoff
         // contracts are deployed, then destroyed in the same transaction.
         // solhint-disable-next-line
-        selfdestruct(creator);
+        selfdestruct(intent.escrow);
     }
 
-    // One step: receive mintToken and send to creator
-    function receiveAndSelfDestruct() public {
-        require(msg.sender == creator, "FCCTP: only creator");
-
-        uint256 amount = CrepeTokenUtils.getBalanceOf(
-            destination.mintToken.addr,
-            address(this)
-        );
-        require(
-            amount >= destination.mintToken.amount,
-            "FCCTP: insufficient bridge mint token received"
-        );
-
-        // Send to FastCCTP, which will forward to current recipient
-        CrepeTokenUtils.transfer(destination.mintToken.addr, creator, amount);
-
-        // This use of SELFDESTRUCT is compatible with EIP-6780. Handoff
-        // contracts are deployed, then destroyed in the same transaction.
-        // solhint-disable-next-line
-        selfdestruct(creator);
-    }
-
+    /// Accept native-token (eg ETH) inputs
     receive() external payable {}
 }
