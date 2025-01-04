32b5d417a627d8c226e63567de5c0a1c66ceb4d3
DC <dc@dcpos.ch>
2025-01-03 23:49:24 -0800

    dev: move clippy

--- apps/daimo-clippy/src/env.ts
+++
@@ -1,8 +0,0 @@
-import { DaimoChain, getChainConfig } from "@daimo/contract";
-import dotenv from "dotenv";
-dotenv.config();
-
-// || prevents build failures during CI
-export const chainConfig = getChainConfig(
-  (process.env.DAIMO_CHAIN || "baseSepolia") as DaimoChain,
-);
